b7ab453047544235491c3a7573ebb0e62aa56339
Lucas Santos <lucas.luchesi1998@gmail.com>
2026-05-02 16:03:49 -0300

    LocalDateAdapter.java

---
+++ src/LocalDateAdapter.java
@@ -0,0 +1,24 @@
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+public class LocalDateAdapter extends TypeAdapter<LocalDate> {
+
+    @Override
+    public void write(JsonWriter out, LocalDate value) throws IOException {
+        if (value == null) {
+            out.nullValue();
+            return;
+        }
+        out.value(value.toString()); // YYYY-MM-DD
+    }
+
+    @Override
+    public LocalDate read(JsonReader in) throws IOException {
+        String date = in.nextString();
+        return LocalDate.parse(date);
+    }
+}
